5d7690015c74e414a6027e0a7dd957715716d011
Jairo Belen <jairo02193@gmail.com>
2024-08-19 12:30:50 -0400

    feat: creado entorno virtual y automatizando instalacion

---
+++ api/db_connect.py
@@ -0,0 +1,26 @@
+import psycopg2
+from psycopg2 import sql
+
+# Conexión a PostgreSQL
+conn = psycopg2.connect(
+    dbname="postgres",
+    user="tu_usuario",
+    password="tu_contraseña",
+    host="localhost",
+    port="5432"
+)
+conn.autocommit = True
+
+# Crear un cursor
+cursor = conn.cursor()
+
+# Crear la base de datos
+db_name = "juegoReto"
+db_password = "2354"
+cursor.execute(sql.SQL("CREATE DATABASE {}").format(sql.Identifier(db_name)))
+
+# Cerrar la conexión
+cursor.close()
+conn.close()
+
+print(f"Base de datos '{db_name}' creada exitosamente.")
